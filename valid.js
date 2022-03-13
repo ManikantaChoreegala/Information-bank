f763e3f4b30f956d9a22b54726bdd2f80d5f4aef
Manikanta <manikantachoreegala2001@gmail.com>
2022-03-13 08:24:39 +0530

    everything is updated

--- valid.js
+++ valid.js
@@ -1,9 +1,9 @@
-// /*Login form validation*/
+/*Login form validation*/
 
 document.addEventListener("DOMContentLoaded", (event) => { 
-    let username = document.getElementById("username")
-    let password = document.getElementById("password")
-    let form = document.getElementById("form")
+    var username = document.getElementById("username")
+    var password = document.getElementById("password")
+    var form = document.getElementById("form")
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         loginvalid();
@@ -30,11 +30,94 @@ function loginvalid(){
     }
 
     else{
+        alert("Login Success")
         window.location.href = "/bankpage.html"
     }
 
 
 }
 
-// let submitbtn = document.getElementById("submit-btn");
-// submitbtn.addEventListener("click", loginvalid());
+
+//Registration form validation
+
+document.addEventListener("DOMContentLoaded", (event) => { 
+   
+    var firstname = document.getElementById("firstname")
+    var lastname = document.getElementById("lastname")
+    var email = document.getElementById("email")
+
+    var registerPassword = document.getElementById("registerPassword")
+
+    var confirmPassword = document.getElementById("confirmPassword")
+
+    var phoneNumber = document.getElementById("phoneNumber")
+
+    var address = document.getElementById("address")
+
+    var registerForm = document.getElementById("registerForm")
+    
+   
+    registerForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        registration();
+    })
+
+});
+
+
+function registration(){
+    if(firstname.value.trim() == ""){
+        alert("Please enter your first name");       
+        return false;
+    }
+
+    else if(lastname.value.trim() == ""){
+        alert("Please enter your last name");          
+        return false;
+    }
+
+    else if(email.value.trim() == ""){
+        alert("Please enter your email");          
+        return false;
+    }
+
+    else if(registerPassword.value.trim() == ""){
+        alert("Please enter your password");          
+        return false;
+    }
+
+    
+
+    else if(confirmPassword.value.trim() == ""){
+        alert("Please Confirm your password");          
+        return false;
+    }
+
+    else if(registerPassword.value.trim().length < 8){
+        alert("Password must contain 8 characters");            
+        return false;
+    }
+
+
+    else if(registerPassword.value !== confirmPassword.value){
+        alert(" Confirm Password is now matched");          
+        return false;
+    }
+
+    else if(phoneNumber.value.trim() == ""){
+        alert("Please enter your Phone number");          
+        return false;
+    }
+
+    else if(address.value.trim() == ""){
+        alert("Please enter your Address");          
+        return false;
+    }
+
+    else{
+        alert("Registration Success")
+        window.location.href = "/bankpage.html"
+    }
+
+
+}
