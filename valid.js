8dea54c5170593d21aa3bc7257b257bf5e39a0fc
Manikanta <manikantachoreegala2001@gmail.com>
2022-03-13 15:20:45 +0530

    login n registration updated

--- valid.js
+++ valid.js
@@ -1,123 +1,85 @@
 /*Login form validation*/
 
-document.addEventListener("DOMContentLoaded", (event) => { 
-    var username = document.getElementById("username")
-    var password = document.getElementById("password")
-    var form = document.getElementById("form")
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        loginvalid();
-    })
+document.addEventListener("DOMContentLoaded", (event) => {
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+  var form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    loginvalid();
+  });
 });
 
+function loginvalid() {
+  if (username.value.trim() == "") {
+    alert("Please Enter username");
+    return false;
+  }
 
-
-
-function loginvalid(){
-    if(username.value.trim() == ""){
-        alert("Please fill username");       
-        return false;
-    }
-
-    else if(password.value.trim() == ""){
-        alert("Please fill Password");          
-        return false;
-    }
-
-    else if(password.value.trim().length < 8){
-        alert("Password must contain 8 characters");            
-        return false;
-    }
-
-    else{
-        alert("Login Success")
-        window.location.href = "./bankpage.html"
-    }
-
-
+  if (password.value.trim() == "") {
+    alert("Please Enter password");
+    return false;
+  } else if (password.value.trim().length < 8) {
+    alert("Your password must contain at least 8 characters");
+  } else {
+    window.location.href = "./bankpage.html";
+    alert("Login Success");
+  }
 }
-
 
 //Registration form validation
 
-document.addEventListener("DOMContentLoaded", (event) => { 
-   
-    var firstname = document.getElementById("firstname")
-    var lastname = document.getElementById("lastname")
-    var email = document.getElementById("email")
+document.addEventListener("DOMContentLoaded", (event) => {
+  var firstname = document.getElementById("firstname");
+  var lastname = document.getElementById("lastname");
+  var email = document.getElementById("email");
 
-    var registerPassword = document.getElementById("registerPassword")
+  var registerPassword = document.getElementById("registerPassword");
 
-    var confirmPassword = document.getElementById("confirmPassword")
+  var confirmPassword = document.getElementById("confirmPassword");
 
-    var phoneNumber = document.getElementById("phoneNumber")
+  var phoneNumber = document.getElementById("phoneNumber");
 
-    var address = document.getElementById("address")
+  var address = document.getElementById("address");
 
-    var registerForm = document.getElementById("registerForm")
-    
-   
-    registerForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        registration();
-    })
+  var registerForm = document.getElementById("registerForm");
 
+  registerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    registration();
+  });
 });
 
-
-function registration(){
-    if(firstname.value.trim() == ""){
-        alert("Please enter your first name");       
-        return false;
-    }
-
-    else if(lastname.value.trim() == ""){
-        alert("Please enter your last name");          
-        return false;
-    }
-
-    else if(email.value.trim() == ""){
-        alert("Please enter your email");          
-        return false;
-    }
-
-    else if(registerPassword.value.trim() == ""){
-        alert("Please enter your password");          
-        return false;
-    }
-
-    
-
-    else if(confirmPassword.value.trim() == ""){
-        alert("Please Confirm your password");          
-        return false;
-    }
-
-    else if(registerPassword.value.trim().length < 8){
-        alert("Password must contain 8 characters");            
-        return false;
-    }
-
-
-    else if(registerPassword.value !== confirmPassword.value){
-        alert(" Confirm Password is now matched");          
-        return false;
-    }
-
-    else if(phoneNumber.value.trim() == ""){
-        alert("Please enter your Phone number");          
-        return false;
-    }
-
-    else if(address.value.trim() == ""){
-        alert("Please enter your Address");          
-        return false;
-    }
-
-    else{
-        alert("Registration Success")
-        window.location.href = "./bankpage.html"
-    }
-
-
+function registration() {
+  if (firstname.value.trim() == "") {
+    alert("Please enter your first name");
+    return false;
+  } else if (lastname.value.trim() == "") {
+    alert("Please enter your last name");
+    return false;
+  } else if (email.value.trim() == "") {
+    alert("Please enter your email");
+    return false;
+  } else if (registerPassword.value.trim() == "") {
+    alert("Please enter your password");
+    return false;
+  } else if (confirmPassword.value.trim() == "") {
+    alert("Please Confirm your password");
+    return false;
+  } else if (registerPassword.value.trim().length < 8) {
+    alert("Password must contain 8 characters");
+    return false;
+  } else if (registerPassword.value !== confirmPassword.value) {
+    alert(" Confirm Password is now matched");
+    return false;
+  } else if (phoneNumber.value.trim() == "") {
+    alert("Please enter your Phone number");
+    return false;
+  } else if (address.value.trim() == "") {
+    alert("Please enter your Address");
+    return false;
+  } else {
+    window.location.href = "./bankpage.html";
+    alert("Registration Success");
+  }
 }
